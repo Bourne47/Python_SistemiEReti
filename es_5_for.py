b00301b4e6ce811621f9307f60baa2bb0ad098d5
Leonardo Pollicino <leonardo.pollicino@itiscuneo.eu>
2023-09-30 15:04:08 +0200

    esercizio 5
    vediamo per la prima volta le liste e i vari modi di fare il for

---
+++ es_5_for.py
@@ -0,0 +1,9 @@
+lista = [4, 100, 3, 5, "ciao", print] #lista
+
+#ciclo for C-style
+for i in range(0, len(lista)):
+    print(f"L'elemento {i}-esimo è {lista[i]}")
+
+#ciclo for python-style
+for elemento in lista:
+    print(f"elemento: {elemento}")
